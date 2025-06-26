5e2fb02b5011d9a5dd3735d4770205b84a0077b7
Abhiram <abhiram090601@gmail.com>
2025-06-26 21:43:41 +0530

    feat: create home daily events screen

--- app/src/main/java/com/github/abhiram0106/eventplannertask/core/util/HelperFunctions.kt
+++ app/src/main/java/com/github/abhiram0106/eventplannertask/core/util/HelperFunctions.kt
@@ -3,10 +3,20 @@ package com.github.abhiram0106.eventplannertask.core.util
 import com.github.abhiram0106.eventplannertask.feature_home.domain.model.EventData
 import java.time.LocalDate
 import java.time.LocalTime
+import java.time.Year
 import java.time.format.DateTimeFormatter
 
 fun LocalTime.toDisplayString(): String {
     return this.format(DateTimeFormatter.ofPattern("hh:mm a"))
+}
+
+fun LocalDate.toDisplayText(): String {
+    val currentYear = Year.now().value
+    return if (this.year == currentYear) {
+        this.format(DateTimeFormatter.ofPattern("dd MMM"))
+    } else {
+        this.format(DateTimeFormatter.ofPattern("dd MMM yyyy"))
+    }
 }
 
 fun generateEventList(): List<EventData> {
--- app/src/main/java/com/github/abhiram0106/eventplannertask/feature_home/presentation/HomeRoot.kt
+++ app/src/main/java/com/github/abhiram0106/eventplannertask/feature_home/presentation/HomeRoot.kt
@@ -1,10 +1,13 @@
 package com.github.abhiram0106.eventplannertask.feature_home.presentation
 
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.pager.HorizontalPager
+import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -12,7 +15,9 @@ import androidx.compose.ui.unit.dp
 import com.github.abhiram0106.eventplannertask.core.presentation.UiText
 import com.github.abhiram0106.eventplannertask.core.util.generateEventList
 import com.github.abhiram0106.eventplannertask.feature_home.domain.model.EventData
+import com.github.abhiram0106.eventplannertask.feature_home.presentation.components.DateTopBar
 import com.github.abhiram0106.eventplannertask.feature_home.presentation.components.EventListCard
+import java.time.LocalDate
 
 @Composable
 fun HomeRoot(
@@ -28,20 +33,26 @@ fun HomeScreen(
     modifier: Modifier = Modifier,
     events: List<EventData>,
 ) {
-    LazyColumn(
-        modifier = modifier.fillMaxSize(),
-        contentPadding = PaddingValues(vertical = 10.dp),
-        verticalArrangement = Arrangement.spacedBy(4.dp)
-    ) {
-        items(
-            items = events
-        ) { event ->
-            EventListCard(
-                time = event.time,
-                title = event.title,
-                description = event.description,
-                onClick = {}
-            )
+    Column {
+        DateTopBar(
+            date = LocalDate.now(),
+            onSelectDate = {}
+        )
+        LazyColumn(
+            modifier = modifier.fillMaxSize(),
+            contentPadding = PaddingValues(vertical = 10.dp),
+            verticalArrangement = Arrangement.spacedBy(4.dp)
+        ) {
+            items(
+                items = events
+            ) { event ->
+                EventListCard(
+                    time = event.time,
+                    title = event.title,
+                    description = event.description,
+                    onClick = {}
+                )
+            }
         }
     }
 }
