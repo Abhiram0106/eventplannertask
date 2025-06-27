5dc1af4cab3198de237e5583799ea66956590993
Abhiram <abhiram090601@gmail.com>
2025-06-27 10:54:18 +0530

    build: add room and ksp

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
     alias(libs.plugins.jetbrains.kotlin.serialization)
+    alias(libs.plugins.ksp)
 }
 
 android {
@@ -62,4 +63,8 @@ dependencies {
     implementation(libs.kotlinx.serialization.json)
 
     implementation(libs.calendar.composable)
+
+    implementation(libs.room)
+    ksp(libs.room.ksp)
+    implementation(libs.room.ktx)
 }
